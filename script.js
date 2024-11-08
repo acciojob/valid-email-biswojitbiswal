b9dada7e9caaa560c95545d9c49f78c1f2d3c248
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-11-08 16:43:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function validEmail(str: string): boolean {
   //your JS code here.
-	const regxEmail = /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\.[a-zA-Z]{2,})+$/;
-
-	return regxEmail.test(str);
+	const regxEmail = /^[a-zA-Z0-9]+([._-][a-zA-Z0-9]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z]{2,})+$/;
+  return regxEmail.test(str); 
 }
 
 // Do not change the code below.
